cb638467846a7d8dfb095dc557bc03043fbb2bac
Héctor Buruca <torrezhectorb@gmail.com>
2022-03-11 13:02:53 -0600

    todo funciona menos welcome ya que lo borre sin querer

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Routing\Controller as BaseController;
+
+class TestController extends BaseController{
+
+    public function index(){
+        // return "Mi primer controlador";
+        return view("Test/index")->with('title','Mi primer aplicacion con Laravel');
+    }
+
+}
